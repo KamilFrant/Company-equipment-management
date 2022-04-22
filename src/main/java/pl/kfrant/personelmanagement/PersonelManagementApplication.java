138d171cd9ac00308ce835d41c1c8b58dad7796e
Kamil Frant <kamilfrantmu@wp.pl>
2022-04-22 15:19:56 +0200

    Init commit

---
+++ src/main/java/pl/kfrant/personelmanagement/PersonelManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.kfrant.personelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kfrant/personelmanagement/PersonelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kfrant.personelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
