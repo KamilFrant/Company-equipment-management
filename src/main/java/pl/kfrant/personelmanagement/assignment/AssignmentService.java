ae3d18ac63f53c8d2506fbd70accb2c1bcefceb9
Kamil Frant <kamilfrantmu@wp.pl>
2022-04-23 19:11:44 +0200

    Add test data

---
+++ src/main/java/pl/kfrant/personelmanagement/assignment/AssignmentService.java
@@ -0,0 +1,2 @@
+package pl.kfrant.personelmanagement.assignment;public class AssignmentService {
+}
---
+++ src/main/java/pl/kfrant/personelmanagement/assignment/dto/AssignmentDto.java
@@ -0,0 +1,4 @@
+package pl.kfrant.personelmanagement.assignment;
+
+public class AssignmentDto {
+}
